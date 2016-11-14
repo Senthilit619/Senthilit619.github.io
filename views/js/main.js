61142fa003610daaddee31f2d1f8f9e48dfa1e3b
senthil <krsenthilit619@gmail.com>
2016-11-14 16:31:30 +0530

    percent

--- views/js/main.js
+++ views/js/main.js
@@ -449,7 +449,6 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-      var newwidth;
       switch(size):
       {
         case "1":
@@ -464,7 +463,7 @@ var resizePizzas = function(size) {
       }
       var randomPizzas=document.querySelectorAll(".randomPizzaContainer");
     for (var i = 0; i < randomPizzas.length; i++) {
-      randomPizzas[i].style.width = newwidth;
+      randomPizzas[i].style.width = newwidth + "%";
     }
   }
 
