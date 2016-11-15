bb3429ad56a3d4c3f545bbdc03065532a9384f16
senthil <krsenthilit619@gmail.com>
2016-11-15 10:17:31 +0530

    original style

--- views/js/main.js
+++ views/js/main.js
@@ -515,7 +515,7 @@ function updatePositions() {
   var scrollposition = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((scrollposition / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft+100*phase+'px';
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
